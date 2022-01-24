fd6383f740bffddaa7bbc2a68131ef980d003b2d
rishav ranjan <rishav ranjan@DESKTOP-EVVE8QT>
2022-01-24 21:30:37 +0530

    This is 1st commit

---
+++ src/test/java/demoGit/DemoGit.java
@@ -0,0 +1,13 @@
+package demoGit;
+
+import org.testng.annotations.Test;
+
+public class DemoGit {
+	
+	@Test
+	public void demoGit()
+	{
+		System.out.println("This is user1 1st commit ");
+	}
+
+}
